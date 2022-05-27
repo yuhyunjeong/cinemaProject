61ea51a680ad98a91a616f0fd7175d15fb23459e
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-05-27 14:14:14 +0900

    ManagerController에 requestMapping 추가

--- src/main/java/kosta/mvc/controller/ManagerController.java
+++ src/main/java/kosta/mvc/controller/ManagerController.java
@@ -1,8 +1,12 @@
 package kosta.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/manager")
 public class ManagerController {
 
+	@RequestMapping("/managerMain")
+	public void main() {}
 }
