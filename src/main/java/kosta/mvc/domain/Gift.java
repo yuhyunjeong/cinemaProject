bc3651df67173bbc17b230a7f1bd90f2a7ff67b8
subin <xoxosu0305@naver.com>
2022-05-30 14:56:20 +0900

    domain/Gift 생성

---
+++ src/main/java/kosta/mvc/domain/Gift.java
@@ -0,0 +1,33 @@
+package kosta.mvc.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Gift {
+	
+	@Id
+	@NonNull
+	private String giftCode;
+	
+	@OneToOne
+	private String movieCode;
+	
+	@NonNull
+	private String giftName;
+	
+	@NonNull
+	private int giftQTY;
+
+}
