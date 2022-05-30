73c135d71e426c7544b0d54c941a91b0616ec80d
woojin <jungwj92@gmail.com>
2022-05-30 20:55:18 +0900

    index -> cinema/main 변경 testController -> HomeController 변경

--- src/main/java/kosta/mvc/controller/HomeController.java
+++ src/main/java/kosta/mvc/controller/HomeController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class TestController {
+public class HomeController {
 
 	@RequestMapping("/")
 	public String index() {
 		System.out.println("test");
-		return "index";
+		return "cinema/main";
 	}
 	
 	
