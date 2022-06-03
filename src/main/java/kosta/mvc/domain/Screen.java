366aeb1773550a1e0264d75fa6cc010fde3e5a4f
JINA\jinak <jinakim1311@gmail.com>
2022-06-03 17:32:51 +0900

    screenRepository, SeatRepository 생성, Screen 아무것도 안받는 생성자 생성

--- src/main/java/kosta/mvc/domain/Screen.java
+++ src/main/java/kosta/mvc/domain/Screen.java
@@ -1,5 +1,6 @@
 package kosta.mvc.domain;
 
+import javax.annotation.Nonnull;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -7,6 +8,7 @@ import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -14,9 +16,11 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class Screen {
 
 	@Id
+	@Nonnull
 	private int screenCode;
 	
 	private int screenRow;
---
+++ src/main/java/kosta/mvc/repository/ScreenRepository.java
@@ -0,0 +1,10 @@
+package kosta.mvc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kosta.mvc.domain.Orders;
+import kosta.mvc.domain.Screen;
+
+public interface ScreenRepository extends JpaRepository<Screen, Integer>{
+
+}
